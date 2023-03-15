10d73945cbe5f9c960f80964e87a66b1093a1af6
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-03-15 21:53:16 +0300

    Add ruff-lsp to lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -37,6 +37,7 @@ return {
             servers = {
                 bashls = {},
                 marksman = {},
+                ruff_lsp = {},
                 yamlls = {},
                 jsonls = {},
                 lua_ls = {
