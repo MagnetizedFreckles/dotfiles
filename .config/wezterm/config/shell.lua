830306e78985b10346eb7737c1013c0b92f5d3b8
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-06-09 01:01:39 +0300

    Change shell configuration for wezterm

--- .config/wezterm/config/shell.lua
+++ .config/wezterm/config/shell.lua
@@ -1,9 +1,9 @@
 local os_name = require("utils.os_name")
 
 if os_name.os_name() == "macos" then
-    return { "/usr/local/bin/fish", "-l" }
+    return { "/usr/bin/env fish", "-l" }
 elseif os_name.os_name() == "linux" then
-    return { "/usr/bin/fish", "-l" }
+    return { "/usr/bin/env fish", "-l" }
 else
     return { "pwsh.exe" }
 end
