ca23143a5975a07fff82b4db784a7cf41ecba6e2
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-06-12 15:59:47 +0300

    Test fish

--- .config/wezterm/config/shell.lua
+++ .config/wezterm/config/shell.lua
@@ -1,7 +1,7 @@
 local os_name = require("utils.os_name")
 
 if os_name.os_name() == "macos" then
-    return { "/opt/local/bin/fish", "-l" }
+    return { "/usr/local/bin/fish", "-l" }
 elseif os_name.os_name() == "linux" then
     return { "/usr/bin/fish", "-l" }
 else
