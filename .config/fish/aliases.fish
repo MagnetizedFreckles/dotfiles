23fdc08196956c4df30f32a1a6b3d5c2620b3e0a
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-06-21 22:27:32 +0300

    Nixos-friendly change in fish alias config

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,5 +1,5 @@
 # Aliases for managing my dotfile bare repo
-alias dfgit='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dfgit='/usr/bin/env git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias dflg='lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' # Lazygit alias
 
 # Abbreviations for shortening frequently used commands
