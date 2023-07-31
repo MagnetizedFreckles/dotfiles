c0fb2fe1f422f34027bd053efa5c5fc8f2ee83c4
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-07-31 22:04:39 +0300

    Add nil_ls to lsp configuration

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -65,6 +65,7 @@ local M = {
                     },
                 },
             },
+            nil_ls = {},
         },
         -- you can do any additional lsp server setup here
         -- return true if you don't want this server to be setup with lspconfig
