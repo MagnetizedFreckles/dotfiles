69ae6801e2b270ea3671922df7cbe9d3dd64525b
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-07-31 22:32:44 +0300

    I hope this works

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -65,7 +65,9 @@ local M = {
                     },
                 },
             },
-            nil_ls = {},
+            nil_ls = {
+                mason = false,
+            },
         },
         -- you can do any additional lsp server setup here
         -- return true if you don't want this server to be setup with lspconfig
