686df2a0b559e37fc1bde16a7fe6ac1fc41ac102
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-08-07 04:30:42 +0300

    Add aliases to shell_common

--- .shells_common.sh
+++ .shells_common.sh
@@ -1,10 +1,18 @@
 # Aliases
 alias la="ls -lah"
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
 alias scim="sc-im"
 alias yay="paru"
 alias syu="paru -Syu"
 alias vpn="wg-quick up $HOME/.wireguard/*-ams.conf"
 alias vpndown="wg-quick down $HOME/.wireguard/*-ams.conf"
+
 
 # Editing crontab with neovim
 alias crontab="export VISUAL=nvim; crontab"
