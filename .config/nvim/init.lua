abc052023d25edb50bd4e97cf117e69daad9d715
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-08-07 04:30:20 +0300

    Configs for neovide? Idk why

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,8 +3,10 @@ require("config.lazy")
 
 if vim.g.neovide then
     -- Put anything you want to happen only in Neovide here
-    vim.opt.guifont = { "FiraCode Nerd Font", "h15" } -- text below applies for VimScript
+    vim.opt.guifont = { "FiraCode Nerd Font", "h15" }
     vim.g.neovide_remember_window_size = true
     vim.g.neovide_cursor_animation_length = 0.05
     vim.g.neovide_cursor_vfx_mode = "pixiedust"
+    vim.g.neovide_transparency = 0.9
+    -- vim.g.neovide_background_color = "#0e1019"
 end
