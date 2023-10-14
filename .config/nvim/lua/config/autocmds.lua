fc2ff380767a2935e2b2e880d8bfdbdc5ba8bba4
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-10-14 23:25:47 +0300

    Add autocmd for 2-space indentations in .nix

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,19 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Two spaces is a Nix-lang convention
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "nix",
+    callback = function()
+        vim.opt.tabstop = 2
+        vim.opt.softtabstop = 2
+        vim.opt.shiftwidth = 2
+    end,
+})
+
+-- I tried setting all of this for rmd, but failed,
+-- and right have zero plans on using, so whatever
+
 --[[ autocmd Filetype rmd map <F5> :!echo<space>"require(rmarkdown);<space>render('<c-r>%')"<space>\|<space>R<space>--vanilla<enter>
 
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
