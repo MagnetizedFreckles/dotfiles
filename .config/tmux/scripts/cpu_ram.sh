dd5762266bd8688099c43ffc6539a80144401172
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-10-31 02:33:09 +0300

    Considerable amount of changes to tmux.conf

--- .config/tmux/scripts/cpu_ram.sh
+++ .config/tmux/scripts/cpu_ram.sh
@@ -2,4 +2,12 @@
 
 # A command which output is displayed in the statusbar of tmux
 
-top -bn1 | awk -vORS=' ' '/^%Cpu/{print "CPU: "100-$8"%"} /^MiB Mem :/{if ($4 == 0) print "RAM: N/A"; else print "RAM: "$8/($4+$8)*100"%"}'
+top -bn1 | awk -v ORS=' ' '/^%Cpu/ {
+    printf "CPU: %.2f%% ", 100 - $8
+}
+/^MiB Mem :/ {
+    if ($4 == 0)
+        printf "RAM: N/A"
+    else
+        printf "RAM: %.2f%%", ($8 / ($4 + $8)) * 100
+}'
