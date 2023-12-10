d239a847fb230c52ac3ff63239f446e57d5e933d
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-12-10 21:05:00 +0300

    Update nerdfont script so now it is less bloated

--- .scripts/nerdfont_download.sh
+++ .scripts/nerdfont_download.sh
@@ -3,41 +3,29 @@
 # Set the target location for unzipping based on the operating system
 OS=$(uname -s)
 if [ "$OS" = "Linux" ]; then
-    TARGET_DIR="$HOME/.local/share/fonts"
+	TARGET_DIR="$HOME/.local/share/fonts"
 elif [ "$OS" = "Darwin" ]; then
-    TARGET_DIR="$HOME/Library/Fonts"
+	TARGET_DIR="$HOME/Library/Fonts"
 else
-    echo "Unsupported operating system: $OS"
-    exit 1
+	echo "Unsupported operating system: $OS"
+	exit 1
 fi
-
-# Define the font release page URL
-RELEASES_URL="https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest"
 
 echo "[-] Downloading FiraCode and Iosevka Nerd fonts [-]"
 
-# Function to get the latest version for a given font release page URL
-get_latest_version() {
-    local RELEASES_URL="$1"
-    local LATEST_VERSION=$(curl -sSL "$RELEASES_URL" | jq -r '.tag_name')
-    echo "$LATEST_VERSION"
-}
-
 # Get the latest version for FiraCode
-FIRACODE_LATEST_VERSION=$(get_latest_version "$RELEASES_URL")
-FIRACODE_DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/$FIRACODE_LATEST_VERSION/FiraCode.zip"
+FIRACODE_DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip"
 
-echo "Downloading FiraCode version $FIRACODE_LATEST_VERSION"
+echo "Downloading FiraCode latest version"
 wget -P "$HOME/Downloads/" "$FIRACODE_DOWNLOAD_URL"
 echo "Unzipping FiraCode"
 unzip -q "$HOME/Downloads/FiraCode.zip" -d "$TARGET_DIR"
 echo "FiraCode installed!"
 
 # Get the latest version for Iosevka
-IOSEVKA_LATEST_VERSION=$(get_latest_version "$RELEASES_URL")
-IOSEVKA_DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/$IOSEVKA_LATEST_VERSION/Iosevka.zip"
+IOSEVKA_DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.zip"
 
-echo "Downloading Iosevka version $IOSEVKA_LATEST_VERSION"
+echo "Downloading Iosevka latest version"
 wget -P "$HOME/Downloads/" "$IOSEVKA_DOWNLOAD_URL"
 echo "Unzipping Iosevka"
 unzip -q "$HOME/Downloads/Iosevka.zip" -d "$TARGET_DIR"
