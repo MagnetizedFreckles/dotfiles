d3fda0810d04cbb244037c4c109590794c4ba9e7
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-12-25 21:55:55 +0300

    Fix borken cmp due to misconfiguration

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,11 +1,10 @@
 return {
     "hrsh7th/nvim-cmp",
-    opts = function()
+    opts = function(_, opts)
         local cmp = require("cmp")
         -- This snippet should enable autocompletion for non-english words
         -- (cyrillic in my case)
-        return {
-            sources = cmp.config.sources({ { name = "buffer", option = { keyword_pattern = [[\k\+]] } } }),
-        }
+        -- TODO: If possible, make it also work for words that start with capital letters
+        opts.sources = cmp.config.sources({ { name = "buffer", option = { keyword_pattern = [[\k\+]] } } })
     end,
 }
