e160817784b8028b03007cefd22d800b78f76551
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-12-30 01:47:43 +0300

    Fix my cmp config

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,10 +1,24 @@
 return {
     "hrsh7th/nvim-cmp",
+    dependencies = { "f3fora/cmp-spell" },
     opts = function(_, opts)
         local cmp = require("cmp")
-        -- This snippet should enable autocompletion for non-english words
-        -- (cyrillic in my case)
-        -- TODO: If possible, make it also work for words that start with capital letters
-        opts.sources = cmp.config.sources({ { name = "buffer", option = { keyword_pattern = [[\k\+]] } } })
+        opts.sources = cmp.config.sources({
+            -- This snippet should enable autocompletion for non-english words
+            -- (cyrillic in my case)
+            -- TODO: If possible, make it also work for words that start with capital letters
+            { name = "buffer", option = { keyword_pattern = [[\k\+]] } },
+            -- Use spelllang dictionaries as source
+            -- TODO: Somehow differentiate spelllang suggestions from buffer
+            {
+                name = "spell",
+                option = {
+                    keep_all_entries = false,
+                    enable_in_context = function()
+                        return true
+                    end,
+                },
+            },
+        })
     end,
 }
