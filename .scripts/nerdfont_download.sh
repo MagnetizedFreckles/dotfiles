ce4a3f07387a6e88917c515841477c93eaefce08
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2023-12-30 01:45:07 +0300

    Cleanup script, add Monaspace

--- .scripts/nerdfont_download.sh
+++ .scripts/nerdfont_download.sh
@@ -3,33 +3,36 @@
 # Set the target location for unzipping based on the operating system
 OS=$(uname -s)
 if [ "$OS" = "Linux" ]; then
-	TARGET_DIR="$HOME/.local/share/fonts"
+	TARGET_DIR="$HOME/.local/share/fonts/"
 elif [ "$OS" = "Darwin" ]; then
-	TARGET_DIR="$HOME/Library/Fonts"
+	TARGET_DIR="$HOME/Library/Fonts/"
 else
 	echo "Unsupported operating system: $OS"
 	exit 1
 fi
 
-echo "[-] Downloading FiraCode and Iosevka Nerd fonts [-]"
+echo "[-] Downloading FiraCode, Iosevka and Monaspace Nerd fonts [-]"
 
-# Get the latest version for FiraCode
-FIRACODE_DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip"
-
-echo "Downloading FiraCode latest version"
+FIRACODE_DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.tar.xz"
+echo "Downloading latest version of FiraCode"
 wget -P "$HOME/Downloads/" "$FIRACODE_DOWNLOAD_URL"
 echo "Unzipping FiraCode"
-unzip -q "$HOME/Downloads/FiraCode.zip" -d "$TARGET_DIR"
+tar -xf "$HOME/Downloads/FiraCode.tar.xz" -C "$TARGET_DIR" --xz
 echo "FiraCode installed!"
 
-# Get the latest version for Iosevka
-IOSEVKA_DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.zip"
-
-echo "Downloading Iosevka latest version"
+IOSEVKA_DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.tar.xz"
+echo "Downloading latest version of Iosevka"
 wget -P "$HOME/Downloads/" "$IOSEVKA_DOWNLOAD_URL"
 echo "Unzipping Iosevka"
-unzip -q "$HOME/Downloads/Iosevka.zip" -d "$TARGET_DIR"
+tar -xf "$HOME/Downloads/FiraCode.tar.xz" -C "$TARGET_DIR" --xz
 echo "Iosevka installed!"
+
+MONASPACE_DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Monaspace.tar.xz"
+echo "Downloading latest version of Monaspace"
+wget -P "$HOME/Downloads/" "$MONASPACE_DOWNLOAD_URL"
+echo "Unzipping Monaspace"
+tar -xf "$HOME/Downloads/Monaspace.tar.xz" -C "$TARGET_DIR" --xz
+echo "Monaspace installed!"
 
 fc-cache -fv
 echo "That's all! You may want to clear the Downloads now"
