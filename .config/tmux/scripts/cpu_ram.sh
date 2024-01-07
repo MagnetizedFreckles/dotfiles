65a7405d33ab678370e2092ade9755824e9bf9f4
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2024-01-07 23:40:27 +0300

    Remove decimals for conciseness

--- .config/tmux/scripts/cpu_ram.sh
+++ .config/tmux/scripts/cpu_ram.sh
@@ -3,11 +3,11 @@
 # A command which output is displayed in the statusbar of tmux
 
 top -bn1 | awk -v ORS=' ' '/^%Cpu/ {
-    printf "CPU: %.2f%% ", 100 - $8
+    printf "CPU: %.0f%% ", 100 - $8
 }
 /^MiB Mem :/ {
     if ($4 == 0)
         printf "RAM: N/A"
     else
-        printf "RAM: %.2f%%", ($8 / ($4 + $8)) * 100
+        printf "RAM: %.0f%%", ($8 / ($4 + $8)) * 100
 }'
