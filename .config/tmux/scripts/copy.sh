15991d1897f001981bd673bd232da11ba1a4c6d1
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2024-03-10 23:44:25 +0300

    Tmux now correctly copies under wayland (i hope)

--- .config/tmux/scripts/copy.sh
+++ .config/tmux/scripts/copy.sh
@@ -4,9 +4,15 @@
 
 OS=$(uname -s)
 if [ "$OS" = "Linux" ]; then
-	xsel -ip && xsel -op | xsel -ib
-	#cb copy
-	tmux display-message "Copied!"
+	# Works in my case, but here is another way of checking, may be useful:
+	# https://unix.stackexchange.com/questions/202891/how-to-know-whether-wayland-or-x11-is-being-used
+	if [ "$XDG_SESSION_TYPE" = "x11" ]; then
+		xsel -ip && xsel -op | xsel -ib
+		tmux display-message "Copied!"
+	elif [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+		wl-copy
+		tmux display-message "Copied!"
+	fi
 elif [ "$OS" = "Darwin" ]; then
 	reattach-to-user-namespace pbpaste | reattach-to-user-namespace pbcopy
 	tmux display-message "Copied!"
--- .config/tmux/scripts/paste.sh
+++ .config/tmux/scripts/paste.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
 # A script to paste in tmux buffer
+# This mostly DOESN'T work. And I honestly have zero need in this script.
+# Ctrl-Shift-V does the good enough job
 
 OS=$(uname -s)
 if [ "$OS" = "Linux" ]; then
-	xclip -o | tmux load-buffer -
-	#cb paste | tmux load-buffer -
-	tmux paste-buffer
-	tmux display-message "Pasted!"
+	# Works in my case, but here is another way of checking, may be useful:
+	# https://unix.stackexchange.com/questions/202891/how-to-know-whether-wayland-or-x11-is-being-used
+	if [ "$XDG_SESSION_TYPE" = "x11" ]; then
+		xclip -o | tmux load-buffer -
+		tmux paste-buffer
+		tmux display-message "Pasted!"
+	elif [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+		wl-paste | tmux load-buffer -
+		tmux paste-buffer
+		tmux display-message "Pasted!"
+	fi
 elif [ "$OS" = "Darwin" ]; then
 	reattach-to-user-namespace pbpaste | tmux load-buffer -
 	tmux paste-buffer
