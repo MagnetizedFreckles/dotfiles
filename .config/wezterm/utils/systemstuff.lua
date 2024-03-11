0309de00d237b73a9cc9bfaf00ca573cde0c376a
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2024-03-11 15:30:03 +0300

    Lol i forgor to add that

---
+++ .config/wezterm/utils/systemstuff.lua
@@ -0,0 +1,37 @@
+local wezterm = require("wezterm")
+
+local M = {}
+
+function M.os_name()
+    if wezterm.target_triple == "x86_64-apple-darwin" or wezterm.target_triple == "aarch64-apple-darwin" then
+        return "macos"
+    elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+        return "linux"
+    else
+        return "windows"
+    end
+end
+
+function M.windowsystem()
+    local handle = io.popen("loginctl show-session $(loginctl | grep $(whoami) | awk '{print $1}') -p Type")
+    if handle then
+        local result = handle:read("*a")
+        handle:close()
+
+        if result then
+            if string.find(result, "Type=wayland") then
+                return "wayland"
+            elseif string.find(result, "Type=x11") then
+                return "x11"
+            else
+                return "error lol"
+            end
+        else
+            return "error lol"
+        end
+    else
+        return "error lol"
+    end
+end
+
+return M
