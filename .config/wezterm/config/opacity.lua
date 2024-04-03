3eb8a83f63d05ae93436641fd325548608792a35
MagnetizedFreckles <magnetizedfreckles@disroot.org>
2024-04-03 23:06:09 +0300

    Forceblur died again

--- .config/wezterm/config/opacity.lua
+++ .config/wezterm/config/opacity.lua
@@ -4,7 +4,7 @@ if systemstuff.os_name() == "linux" then
     if systemstuff.windowsystem() == "x11" then
         return 0.90
     elseif systemstuff.windowsystem() == "wayland" then
-        return 0.95
+        return 0.98
     end
 elseif systemstuff.os_name() == "macos" then
     return 0.80
